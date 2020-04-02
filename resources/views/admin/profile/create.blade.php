dbec9b5960f2b95d829ab649b62611f01d85aa6b
tokunaga <ikimono.polka00@gmail.com>
2020-04-02 14:22:31 +0000

    Laravel課題

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8" />
+         <title>TECH BOOST</title>
+    </head>
+    <body>
+    <h1>Hallo world!!!</h1>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
+    <ol>
+        <li>one</li>
+        <li>two</li>
+        <li>three</li>
+    </ol>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8" />
+         <title>TECH BOOST</title>
+    </head>
+    <body>
+    <h1>Hallo world!!!</h1>
+    <ul>
+        <li>a</li>
+        <li>b</li>
+    </ul>
+    <ol>
+        <li>un</li>
+        <li>deux</li>
+        <li>trois</li>
+    </ol>
+    </body>
+</html>
